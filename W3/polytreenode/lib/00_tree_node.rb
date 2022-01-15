fa13d8273823d6d808adad1db985607a2ff304d2
Catherine Choi <catherinemargaretchoi@gmail.com>
2022-01-14 19:42:02 -0500

    Add #dfs for PolyTreeNote (and more)

--- W3/polytreenode/lib/00_tree_node.rb
+++ W3/polytreenode/lib/00_tree_node.rb
@@ -22,4 +22,28 @@ class PolyTreeNode
         end
     end
 
+    def add_child(child_node)
+        child_node.parent=(self)
+    end
+
+    def remove_child(child_node)
+        raise "Error" if !self.children.include?(child_node)
+        child_node.parent=(nil)
+    end
+
+    def dfs(target_value)
+        return self if self.value == target_value
+
+        children.each do |child|
+            child_val = child.dfs(target_value)
+            return child_val if child_val != nil
+        end
+        nil
+    end
+
 end
+
+
+#   1
+# 2   3
+#       4
