0e73e6544ae1dbacd62544bc8caabde8b2ba2855
kingbloopy <kingbloopy@gmail.com>
2022-01-18 16:04:47 -0800

    add bonus recursion

--- W4D2/manager.rb
+++ W4D2/manager.rb
@@ -1,26 +1,41 @@
 require_relative "employee"
 
 class Manager < Employee
+  attr_accessor :employees
+
   def initialize(name, title, salary, boss)
     super(name, title, salary, boss) # manager might have a higher level manager
     @employees = []
   end
 
   def bonus(multiplier)
+    self.total * multiplier
+  end
+
+  # employees are instances
+# [(Ned, 1,000,000, Founder, nil, employees= Darren), 
+# (Darren	$78,000	TA Manager	Ned, employess= Shawna, David), 
+# (Shawna	$12,000	TA	Darren, employees=nil),
+# (David	$10,000	TA	Darren, employees=nil)]
+
+  def total
     sum = 0
-    # queue = employees.dup
-
-    # until queue.empty?
-    #   current_employee = queue.pop
-
-    employees.each do |employee|
-      if employee.employees.nil?
+    
+    employees.each do |employee| # (Ned, 1,000,000, Founder, nil)
+      # if employee.employees.nil? || employee.employees.empty?
+      if employee.is_a?(Employee)
         sum += employee.salary
       else
-        sum += employee.bonus(multiplier)
+        sum += employee.salary
+        sum += employee.total
       end
     end
-
-    sum * multiplier
+    
+    sum 
   end
 end
+
+# queue = employees.dup
+
+# until queue.empty?
+#   current_employee = queue.pop
