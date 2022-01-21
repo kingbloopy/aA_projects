bf2f8308677593405a2038e0ebc2c5d603f19ee8
Cody Degraffe-Niles <codypdniles@gmail.com>
2022-01-21 18:40:24 -0500

    Started building max range

--- W4W5/max_range.rb
+++ W4W5/max_range.rb
@@ -12,7 +12,43 @@ def naive(arr, w) # [1, 2, 3, 5] 3
   current_max_range
 end
 
-p naive([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
-p naive([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
-p naive([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
-p naive([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
+# p naive([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
+# p naive([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
+# p naive([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
+# p naive([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
+
+
+class MyQueue
+
+  def initialize
+    @underlying_array = []
+  end
+
+  def enqueue(ele)
+    @underlying_array << ele
+    ele
+  end
+
+  def dequeue
+    @underlying_array.shift
+  end
+
+  def peek
+    @underlying_array.first
+  end
+
+  def size
+    @underlying_array.length
+  end
+
+  def empty?
+    size == 0
+  end
+
+end
+
+class MyStack
+
+  
+
+end
