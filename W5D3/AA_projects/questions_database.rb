f66d1b0772955605a3ef9287f0fb16be982b9beb
johnnyhoang510 <johnnyhoang510@yahoo.com>
2022-01-26 14:07:37 -0800

    added class methods

--- W5D3/AA_projects/questions_database.rb
+++ W5D3/AA_projects/questions_database.rb
@@ -4,5 +4,48 @@ require 'singleton'
 
 class QuestionsDatabase < SQLite3::Database
   include Singleton
-  
+
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+class Users
+  def self.all
+    data = QuestionsDatabase.instance.execute('SELECT * FROM users')
+    data.map { |datum| Users.new(datum) }
+  end
+
+  def self.find_by_name(fname, lname)
+    QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        fname = ?
+      AND
+        lname = ?
+    SQL
+  end
+
+  def self.find_by_id(id)
+    QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        users
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+
 end
