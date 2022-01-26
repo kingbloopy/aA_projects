011e0793dbdcdbeca9798a20594243cb1c516a39
kingbloopy <kingbloopy@gmail.com>
2022-01-26 11:16:36 -0800

    we did it '

--- W5D3/AA_projects/import_db.sql
+++ W5D3/AA_projects/import_db.sql
@@ -9,14 +9,29 @@ CREATE TABLE questions(
   id INTEGER PRIMARY KEY,
   title TEXT NOT NULL,
   body TEXT NOT NULL,
+  author_id INTEGER NOT NULL,
 
   FOREIGN KEY (author_id) REFERENCES users(id) /* associated authors */
 );
 
 CREATE TABLE question_follows(
   id INTEGER PRIMARY KEY,
-  users_id INTEGER NOT NULL,
-  questions_id INTEGER NOT NULL
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
+CREATE TABLE replies(
+  id INTEGER PRIMARY KEY,
+  question_subject_id INTEGER NOT NULL,
+  parent_id INTEGER, 
+  reply_user_id INTEGER NOT NULL,
+  body TEXT NOT NULL,
+
+FOREIGN KEY (question_subject_id) REFERENCES questions(id),
+FOREIGN KEY (reply_user_id) REFERENCES users(id),
+FOREIGN KEY (parent_id) REFERENCES replies(id)
+);
 
