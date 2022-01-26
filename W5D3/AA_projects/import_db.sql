723495929f0cad4197744acc256c18b7c186148e
johnnyhoang510 <johnnyhoang510@yahoo.com>
2022-01-26 12:04:18 -0800

    inserted values

--- W5D3/AA_projects/import_db.sql
+++ W5D3/AA_projects/import_db.sql
@@ -1,3 +1,4 @@
+PRAGMA foreign_keys = ON;
 
 CREATE TABLE users(
   id INTEGER PRIMARY KEY, 
@@ -34,4 +35,48 @@ FOREIGN KEY (question_subject_id) REFERENCES questions(id),
 FOREIGN KEY (reply_user_id) REFERENCES users(id),
 FOREIGN KEY (parent_id) REFERENCES replies(id)
 );
+
+CREATE TABLE question_likes(
+  id INTEGER PRIMARY KEY,
+  likes INTEGER,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id),
+);
+
+
+INSERT INTO
+  users (fname, lname)
+VALUES 
+  ('Oogie', 'Booger'),
+  ('Pickle', 'Rick');
+
+
+INSERT INTO
+  questions (title, body, author_id)
+VALUES
+  ('Colors', 'What color is the sun?', 1)
+  ('Pickle', 'How do I become a pickle?', 2);
+
+INSERT INTO
+  question_follows (user_id, question_id)
+VALUES
+  (1, 1),
+  (2, 2);
+
+INSERT INTO
+  replies (question_subject_id, parent_id, reply_user_id, body)
+VALUES
+  (1, 1, 2, 'Yellow'),
+  (2, 1, 1, 'Watch Rick and Morty');
+
+INSERT INTO
+  question_likes (likes, user_id, question_id)
+VALUES
+  (33, 1, 1),
+  (5000, 2, 2);
+
+
 
