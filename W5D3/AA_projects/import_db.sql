579de3575e853417336abb9112eadd84928ab3e2
johnnyhoang510 <johnnyhoang510@yahoo.com>
2022-01-26 10:42:45 -0800

    added new tables

--- W5D3/AA_projects/import_db.sql
+++ W5D3/AA_projects/import_db.sql
@@ -3,4 +3,20 @@ CREATE TABLE users(
   id INTEGER PRIMARY KEY, 
   fname TEXT NOT NULL,
   lname TEXT NOT NULL
-)
+);
+
+CREATE TABLE questions(
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+
+  FOREIGN KEY (author_id) REFERENCES users(id) /* associated authors */
+);
+
+CREATE TABLE question_follows(
+  id INTEGER PRIMARY KEY,
+  users_id INTEGER NOT NULL,
+  questions_id INTEGER NOT NULL
+);
+
+
