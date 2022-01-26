e6ebd41fc30154acb603ee8d3618f16562e234d7
kingbloopy <kingbloopy@gmail.com>
2022-01-26 14:27:52 -0800

    we gettin it!'

--- W5D3/AA_projects/questions_database.rb
+++ W5D3/AA_projects/questions_database.rb
@@ -13,6 +13,9 @@ class QuestionsDatabase < SQLite3::Database
 end
 
 class Users
+
+  attr_accessor :fname, :lname, :id
+
   def self.all
     data = QuestionsDatabase.instance.execute('SELECT * FROM users')
     data.map { |datum| Users.new(datum) }
@@ -46,6 +49,98 @@ class Users
     @id = options['id']
     @fname = options['fname']
     @lname = options['lname']
+  end
+
+end
+
+class Questions
+
+  attr_accessor :id, :title, :body, :author_id
+
+  def self.all
+    data = QuestionsDatabase.instance.execute('SELECT * FROM questions')
+    data.map { |datum| Questions.new(datum) }
+  end
+
+  def self.find_title(title)
+    QuestionsDatabase.instance.execute(<<-SQL, title)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        title = ?
+    SQL
+  end
+
+  def self.find_by_id(id)
+    QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        questions
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @title = options['title']
+    @body = options['body']
+    @author_id = options['author_id']
+  end
+end
+
+class QuestionFollows
+
+  attr_accessor :id, :user_id, :question_id
+
+  def self.all
+    data = QuestionsDatabase.instance.execute('SELECT * FROM question_follows')
+    data.map { |datum| QuestionFollows.new(datum) }
+  end
+
+  def self.find_by_id(id)
+    QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        question_follows
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @user_id = options['user_id']
+    @question_id = options['question_id']
+  end
+
+end
+
+class Replies
+
+  attr_accessor :id, :question_subject_id, :parent_id, :reply_user_id, :body
+
+  def self.find_by_id(id)
+    QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        replies
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @question_subject_id = options['question_subject_id']
+    @parent_id = options['parent_id']
+    @reply_user_id = options['reply_user_id']
+    @body = options['body']
   end
 
 end
