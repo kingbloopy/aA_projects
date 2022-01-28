3d1f0aec15e6372b3e926bce9d97ece328bb86a0
Elhoussine Elouardy <elouardy.elhoussine@gmail.com>
2022-01-28 15:42:43 -0800

    Finished 01 querie

--- W5D5/movie_buff/movie_buff/01_queries.rb
+++ W5D5/movie_buff/movie_buff/01_queries.rb
@@ -49,7 +49,12 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
-
+  Movie
+    .select(:id, :title)
+    .joins(:castings)
+    .group('movies.id')
+    .order("COUNT(castings.actor_id) DESC")
+    .limit(3)
 end
 
 def directed_by_one_of(them)
@@ -65,6 +70,11 @@ def directed_by_one_of(them)
   #
   # Find the id and title of all the movies directed by one of 'them'.
 
+  Movie
+    .select(:id, :title)
+    .joins(:director)
+    .where(actors: {name: them})
+    # .where("actors.name IN (?)", them)
 end
 
 def movie_names_before_1940
@@ -78,5 +88,7 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
-
+  Movie
+    .where('yr < 1940')
+    .pluck(:title)
 end
