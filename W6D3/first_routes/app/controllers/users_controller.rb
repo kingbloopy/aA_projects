2cbb37ddf1e51dd5b442de3ec676969202b390d0
Zachary Werbalowsky <zachwerbalowsky@Zacharys-MacBook-Pro.local>
2022-02-02 15:27:27 -0800

    Update dstryyy

--- W6D3/first_routes/app/controllers/users_controller.rb
+++ W6D3/first_routes/app/controllers/users_controller.rb
@@ -5,14 +5,39 @@ class UsersController < ApplicationController
     end
 
     def create 
-        render json: params
+        user = User.new(params.require(:user).permit(:name, :email))
+        if user.save
+            render json: user
+        else 
+            render json: user.errors.full_messages, status: 422
+
+        end 
+        
     end
 
     def show
-        render json: params
+        user = User.find(params[:id])
+        render json: user
     end
 
+    def update
+        user = User.find(params[:id])
+        if  user.update(:name, :email)
+            redirect_to "/users/#{user.id}"
+        else
+            render json: user.errors.full_messages, status: 422
 
+        end
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        if user.destroy
+            render json: 'U got fuckin destroyed'
+        else
+            render json: user.errors.full_messages, status: 422
+        end
+    end
     #params[:id]
 
     private 
