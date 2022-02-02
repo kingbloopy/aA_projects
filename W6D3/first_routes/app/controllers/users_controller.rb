c7eddd84f1674d631347e9259cce1771dfcaf881
Zachary Werbalowsky <zachwerbalowsky@Zacharys-MacBook-Pro.local>
2022-02-02 14:14:20 -0800

    Update pizza party

--- W6D3/first_routes/app/controllers/users_controller.rb
+++ W6D3/first_routes/app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
-  
+    def index 
+        #users = User.all
+        render plain: 'will this come up?' #json: users
+    end
+
+    def create 
+        render json: params
+    end
+
+
+
+    #params[:id]
+
+    private 
+
 end
