c9b0b1a8be3d45d8d5f554b627f65866e40a681e
kingbloopy <kingbloopy@gmail.com>
2022-02-04 14:40:36 -0800

    kitty time

---
+++ W6D5/ninety_nine_cats/app/models/cat.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  color       :string           not null
+#  birth_date  :date             not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+require 'date'
+
+class Cat < ApplicationRecord
+  COLORS = ['rainbow', 'unicorn', 'purple']
+  validates :name, :sex, :color, :birth_date, :description, presence: true
+  validates :color, inclusion: {in: COLORS}
+  validates :sex, inclusion: {in: ['M', 'F']}
+
+  def age
+    2022 - self.birth_date.year
+  end
+end
---
+++ W6D5/ninety_nine_cats/db/seeds.rb
@@ -0,0 +1,18 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveRecord::Base.transaction do 
+  Cat.destroy_all
+
+  cat1 = Cat.create!(name: 'Bob', sex: 'M', color: 'purple', birth_date: '2020/01/20', description: 'fluffy boi')
+
+  cat2 = Cat.create!(name: 'Wiggles', sex: 'F', color: :rainbow, birth_date: '2020/12/20', description: 'fluffiest')
+
+  cat3 = Cat.create!(name: 'Joe', sex: 'M', color: 'rainbow', birth_date: '2019/01/20', description: 'smooth boi')
+
+end
