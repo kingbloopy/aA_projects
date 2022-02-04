61ed83f9d8f854fd377d1b2a7d5ad32fb9a64262
Zachary Werbalowsky <zachwerbalowsky@Zacharys-MacBook-Pro.local>
2022-02-03 17:31:25 -0800

    Update

--- W6D3/first_routes/app/controllers/users_controller.rb
+++ W6D3/first_routes/app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
     def index 
-        users = User.all
-        render json: users
+        user = User.find(params[:username])
+        if !user.nil?
+            render json: user
+        else
+            users = User.all
+            render json: users
+        end
     end
 
     def create 
