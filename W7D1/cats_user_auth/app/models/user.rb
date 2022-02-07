c8fe9eea8f8974654f9e21698a0dc9709297ed1c
Justin Nguyen <judnguyen@ucdavis.edu>
2022-02-07 14:28:23 -0800

    Add onto the user model

--- W7D1/cats_user_auth/app/models/user.rb
+++ W7D1/cats_user_auth/app/models/user.rb
@@ -12,9 +12,28 @@
 class User < ApplicationRecord
   validates :user_name, :password_digest, :session_token, presence: true
   validates :session_token, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  attr_reader :password
+
   after_initialize :ensure_session_token
 
-  
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(user_name: user_name)
+    return nil if user.nil?
+    return user if user.is_password?(password)
+    nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
 
   def reset_session_token!
 
