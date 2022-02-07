b693eac0ae7887eabd11ac93118282093fe3d2dd
Justin Nguyen <judnguyen@ucdavis.edu>
2022-02-07 15:39:27 -0800

    Complete Application Controller model

--- W7D1/cats_user_auth/app/controllers/users_controller.rb
+++ W7D1/cats_user_auth/app/controllers/users_controller.rb
@@ -13,4 +13,9 @@ class UsersController < ApplicationController
       render json: @user.errors.full_messages, status: 422
     end
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_name, :password_digest, :session_token)
+  end
 end
