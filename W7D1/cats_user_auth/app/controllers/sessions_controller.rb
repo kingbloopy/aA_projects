95094488949d9b2c45fd1e030a0532faddbafb22
kingbloopy <kingbloopy@gmail.com>
2022-02-07 16:38:42 -0800

    sessions

--- W7D1/cats_user_auth/app/controllers/sessions_controller.rb
+++ W7D1/cats_user_auth/app/controllers/sessions_controller.rb
@@ -1,10 +1,22 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
+    render :new
   end
 
   def create
+    @user = User.find_by_credentials(params[:user][:user_name], params[:user][:password])
+    if @user
+      login!(@user)
+      redirect_to cats_url
+    else
+      render json: ["Invalid Credentials"], status: 401
+    end
   end
 
   def destroy
+    logout!
+    redirect_to 
   end
+
 end
--- W7D1/cats_user_auth/config/routes.rb
+++ W7D1/cats_user_auth/config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/new'
-  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cats, except: :destroy
   resources :cat_rental_requests, only: [:new, :create] do
@@ -14,6 +9,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
 
   root to: redirect('/cats')
 end
