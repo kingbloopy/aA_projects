04aeda36bb4e18b81001816a1d8bbbd2eeafbd28
kingbloopy <kingbloopy@gmail.com>
2022-02-15 14:51:52 -0800

    transpose

--- W8D1/intro_js/phase_1_arrays.js
+++ W8D1/intro_js/phase_1_arrays.js
@@ -7,7 +7,7 @@ Array.prototype.uniq = function() {
   }
   return uniqArr;
 }
-arr = [0, 2, -4, 6, 3, 3, 6, -2, 5, 4, 7, -6]
+// arr = [0, 2, -4, 6, 3, 3, 6, -2, 5, 4, 7, -6]
 // console.log(arr.uniq())
 
 Array.prototype.twoSum = function() {
@@ -21,4 +21,45 @@ Array.prototype.twoSum = function() {
   }
   return pairs
 }
-console.log(arr.twoSum())
+// console.log(arr.twoSum())
+
+
+Array.prototype.transpose = function(){
+  let transposed = [];
+  for (let i = 0; i < this.length; i++){
+    // transposed.push([])
+    let subArr = [];
+    
+    for (let j = 0; j < this.length; j++){
+      debugger
+      // if (this[j][i] !== undefined){
+        subArr.push(this[j][i]);
+      // }
+    }
+    // if (subArr.length > 0){
+      transposed.push(subArr);
+    // }
+  }
+  return transposed;
+}
+
+// arr = [[1, 2, 3], [1, 2, 3], [1 ,2, 3]];
+arr = [[1, 2, 3], [1, 2, 3, 4, 5], [1, 2, 3], [1, 2, 3, 4, 5], [1, 2, 3]];
+console.log(arr.transpose());
+
+// Array.prototype.newTranspose = function(){
+//   let transposed = [];
+//   for (let i = 0; i < this.length; i++){
+//     transposed.push([]);
+//     // let subArr = [];
+
+//     for (let j = 0; j < this.length; j++){
+//       transposed[i].push(this[j][i]);
+//     }
+//     // transposed.push(subArr);
+//   }
+//   return transposed;
+// }
+
+// arr = [[1, 2, 3], [1, 2, 3], [1 ,2, 3], [1, 2, 3], [1, 2, 3]];
+// console.log(arr.newTranspose());
