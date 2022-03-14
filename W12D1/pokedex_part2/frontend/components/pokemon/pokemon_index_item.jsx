a04274516dce5c4699d4d976ada94de31d8304ed
kingbloopy <kingbloopy@gmail.com>
2022-03-14 14:21:32 -0700

    start proj

---
+++ W12D1/pokedex_part2/frontend/components/pokemon/pokemon_index_item.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PokemonIndexItem = (props) => {
+  return (
+    <li className="pokemon-index-item">
+      <span>{props.pokemon.id}</span>
+      <img src={props.pokemon.imageUrl}/>
+      <span>{props.pokemon.name}</span>
+    </li>
+  )
+}
+
+export default PokemonIndexItem;
