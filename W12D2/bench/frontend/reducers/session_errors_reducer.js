456b5ef5cd02e31992f495317b4f87583078f50a
kingbloopy <kingbloopy@gmail.com>
2022-03-16 09:45:07 -0700

    fixed errors

--- W12D2/bench/frontend/reducers/session_errors_reducer.js
+++ W12D2/bench/frontend/reducers/session_errors_reducer.js
@@ -1,13 +1,11 @@
 import { RECEIEVE_ERRORS, RECEIEVE_CURRENT_USER } from "../actions/session_actions";
 
-const sessionErrorsReducer = (state = {}, action) => {
+const sessionErrorsReducer = (state = [], action) => {
   Object.freeze(state);
   const newState = Object.assign({}, state);
   switch (action.type){
     case RECEIEVE_ERRORS:
-      // return Object.assign({}, state, action.errors);
-      newState.errors = Object.values(action.errors);
-      return newState;
+      return action.errors;
     case RECEIEVE_CURRENT_USER:
       newState.errors = [];
       return newState;
